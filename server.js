8f5943ea91030c649b0838a2ba07d8699fffc6f5
ThomasBranchaud <tabranchaud@wpi.edu>
2025-02-18 15:49:13 -0500

    update3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import mime from "mime";
 dotenv.config();
 
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const url = "mongodb+srv://tabranchaud:tb@cluster0.h4cuw.mongodb.net/";
 const connection = new MongoClient(url);
